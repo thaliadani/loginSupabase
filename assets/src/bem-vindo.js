fc8dedbed8f15f116d23999bf01106ebfed63e5a
Thalia Danielle <thaliadani2@gmail.com>
2025-04-01 11:26:26 -0300

    Delete assets/src/bem-vindo.js

--- assets/src/bem-vindo.js
+++
@@ -1,7 +0,0 @@
-const usuarioNome = localStorage.getItem("usuarioNome");
-
-    if (usuarioNome) {
-        document.getElementById("mensagem").innerText = `Bem-vindo(a), ${usuarioNome}!`;
-    } else {
-        document.getElementById("mensagem").innerText = "Usuário não autenticado!";
-    }
